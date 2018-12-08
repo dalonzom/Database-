49c94dec7e1d982af4a450a5bb707a253ed6f118
dalonzom <dalonzo.m@husky.neu.edu>
2018-12-08 12:49:00 -0500

    Update triggers.sql

--- Assignment-2/triggers.sql
+++ Assignment-2/triggers.sql
@@ -1,4 +1,4 @@
-/*Website_role_create*/ 
+DELIMITER $$
 CREATE DEFINER=`dalonzom`@`%` TRIGGER `cs5200`.`website_role_AFTER_INSERT` AFTER INSERT ON `website_role` FOR EACH ROW
 BEGIN
 IF NEW.role = 'owner'   OR NEW.role = 'admin' THEN
@@ -6,7 +6,7 @@ IF NEW.role = 'owner'   OR NEW.role = 'admin' THEN
 	INSERT INTO website_privilege (priviledge_website, webPriviledge_developer_fk, webPriviledge_website_fk) 
     SELECT 'create', websiteRole_developer_fk, websiteRole_website_fk
 		FROM website_role 
-        WHERE websiteRole_developer_fk = NEW.websiteRole_developer_fk AND websiteRole_website_fk = NEW.websiteRole_website_fk; 
+        WHERE websiteRole_developer_fk = NEW.websiteRole_developer_fk AND websiteRole_website_fk = NEW.websiteRole_website_fk;
         
 	INSERT INTO website_privilege (priviledge_website, webPriviledge_developer_fk, webPriviledge_website_fk) 
 	SELECT 'update', websiteRole_developer_fk, websiteRole_website_fk
@@ -45,7 +45,7 @@ IF NEW.role = 'writer' THEN
         WHERE websiteRole_developer_fk = NEW.websiteRole_developer_fk;
 
     END;
- END IF;   
+ END IF;  
 IF NEW.role = 'editor' THEN
 	BEGIN
 INSERT INTO website_privilege (priviledge_website, webPriviledge_developer_fk, webPriviledge_website_fk) 
@@ -58,7 +58,7 @@ INSERT INTO website_privilege (priviledge_website, webPriviledge_developer_fk, w
 		FROM website_role 
         WHERE websiteRole_developer_fk = NEW.websiteRole_developer_fk;
     END;
-END IF; 
+END IF;
 IF NEW.role = 'reviewer' THEN
 BEGIN
 	INSERT INTO website_privilege (priviledge_website, webPriviledge_developer_fk, webPriviledge_website_fk) 
@@ -70,7 +70,8 @@ BEGIN
     
 END IF;
 
-END
+END;
+$$ 
 /*Website_role_update*/ 
 CREATE DEFINER=`dalonzom`@`%` TRIGGER `cs5200`.`website_role_AFTER_UPDATE` AFTER UPDATE ON `website_role` FOR EACH ROW
 BEGIN
@@ -146,14 +147,14 @@ END IF;
 
 
 END
-
+$$
 /*Website_role_delete*/ 
 
 CREATE DEFINER=`dalonzom`@`%` TRIGGER `cs5200`.`website_role_AFTER_DELETE` AFTER DELETE ON `website_role` FOR EACH ROW
 BEGIN
 	DELETE FROM website_privilege WHERE website_privilege.webPriviledge_developer_fk = website_role.websiteRole_developer_fk AND website_role.websiteRole_website_fk = website_privilege.webPriviledge_website_fk; 
 END
-
+$$
 /*Page_role_create*/ 
 CREATE DEFINER=`dalonzom`@`%` TRIGGER `cs5200`.`page_role_AFTER_INSERT` AFTER INSERT ON `page_role` FOR EACH ROW
 BEGIN
@@ -221,6 +222,7 @@ INSERT INTO page_priviledge (priviledge, pagePriviledge_developer_fk, pagePrivil
     END;
 END IF;
 END
+$$
 /*Page_role_update*/ 
 CREATE DEFINER=`dalonzom`@`%` TRIGGER `cs5200`.`page_role_AFTER_UPDATE` AFTER UPDATE ON `page_role` FOR EACH ROW
 BEGIN
@@ -289,8 +291,10 @@ INSERT INTO page_priviledge (priviledge, pagePriviledge_developer_fk, pagePrivil
     END;
 END IF;
 END
+$$
 /*Page_role_delete*/ 
 CREATE DEFINER=`dalonzom`@`%` TRIGGER `cs5200`.`page_role_BEFORE_DELETE` BEFORE DELETE ON `page_role` FOR EACH ROW
 BEGIN
 DELETE FROM page_priviledge WHERE page_role.pageRole_developer_fk = page_priviledge.pagePriviledge_developer_fk   AND page_priviledge.pagePrivildege_page_fk = page_role.pageRole_page_fk; 
 END
+$$
